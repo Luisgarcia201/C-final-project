618f0602f91d9a3a9fc3dfbf4d3199ceb65696a5
Luisgarcia201 <143466517+Luisgarcia201@users.noreply.github.com>
2024-11-27 23:52:00 -0700

    Filled in the rest of my files. Code runs.

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -1,6 +1,40 @@
+// hangman.cpp
 #include "hangman.h"
 #include <iostream>
 
-void Hangman::printHangman() {
-    std::cout << "Hang me!!!" << std::endl;
+Hangman::Hangman(std::string word) : wordToGuess(word), incorrectGuesses(0) {
+    displayWord = std::string(word.length(), '_');  // Initialize the display word with underscores
+}
+
+void Hangman::updateDisplayWord(char guess) {
+    bool correctGuess = false;
+    for (size_t i = 0; i < wordToGuess.length(); ++i) {
+        if (wordToGuess[i] == guess) {
+            displayWord[i] = guess;
+            correctGuess = true;
+        }
+    }
+    if (!correctGuess) {
+        incorrectGuesses++;
+    }
+}
+
+bool Hangman::isWordGuessed() {
+    return displayWord == wordToGuess;
+}
+
+void Hangman::displayWordState() {
+    std::cout << "Current word: " << displayWord << std::endl;
+}
+
+int Hangman::getIncorrectGuesses() {
+    return incorrectGuesses;
+}
+
+std::vector<char> Hangman::getGuessedLetters() {
+    return guessedLetters;
+}
+
+std::string Hangman::getWord() {
+    return wordToGuess;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "GameEngine.h"
 
-int main(int argc, char** argv)
-{
-    std::cout << "Hello!!" << std::endl;
+int main() {
+    std::cout << "Enter your name: ";
+    std::string playerName;
+    std::cin >> playerName;
+
+    GameEngine game(6);
+    game.play();
+
     return 0;
-}  
+}
