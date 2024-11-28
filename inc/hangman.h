4dbf186855576c1b5611a6578e877bf1f63aa8b9
Luisgarcia201 <143466517+Luisgarcia201@users.noreply.github.com>
2024-11-27 23:47:16 -0700

    Filled in hangman.h

--- inc/hangman.h
+++ inc/hangman.h
@@ -1,15 +1,26 @@
 #ifndef HANGMAN_H
 #define HANGMAN_H
 
+#include <vector>
+#include <string>
+
 class Hangman {
-    public:
-    Hangman() = default;
-    ~Hangman() = default;
+private:
+    std::string wordToGuess;
+    std::string displayWord;
+    std::vector<char> guessedLetters;
+    int incorrectGuesses;
 
-    void printHangman();
+public:
+    Hangman(std::string word);
 
-    private:
-    int dummy;
+    void updateDisplayWord(char guess);
+    bool isWordGuessed();
+    void displayWordState();
+    int getIncorrectGuesses();
+    std::vector<char> getGuessedLetters();
+    std::string getWord();
+
 };
 
 #endif
